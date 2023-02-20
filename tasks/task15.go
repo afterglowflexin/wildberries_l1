f3a1c99d6b8b720db7cf23b9225472c7fbb9ab3e
unknown <andru1844@mail.ru>
2023-02-20 18:29:34 +0300

    second commit

--- tasks/task15.go
+++ tasks/task15.go
@@ -7,13 +7,33 @@ import "fmt"
 
 var justString string
 
+// В этом примере функция работает со строкой как с массивом байтов.
+// Для корректного отображения символов, состоящих из более, чем одного байта, необходимо работать с рунами.
+//
+// Также в этой функции может произойти выход за границы массива
+//
+// Не следует работать в функции с глобальной переменной, лучше вовзвращать строку
 func someFunc() {
-	v := "djlsajodjsaopjdopasjopdjasopjdopasjdopjsaopjdopasjodpanvklnaiosbvnioqbiobwpiqk;bvklabklbsiobqbvklqbioqbiowbqibvk"
-	justString = v[:20]
+	v := "ಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕ"
+	justString = v[:10]
+}
+
+// корректный пример
+func someFuncMod() string {
+	v := []rune("ಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕಕ")
+
+	var str string
+
+	if len(v) >= 11 {
+		str = string(v[:10])
+	}
+
+	return str
 }
 
 func main() {
 	someFunc()
+	str := someFuncMod()
 
-	fmt.Println(justString)
+	fmt.Printf("First variant: %v, second variant: %v", justString, str) // First variant: ಕಕಕ�, second variant: ಕಕಕಕಕಕಕಕಕಕ
 }
